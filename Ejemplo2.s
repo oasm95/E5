5cb63d1dba07730111e047eac1d90e3917dca356
oasm95 <41705782+oasm95@users.noreply.github.com>
2019-06-07 16:20:21 -0600

    Update Ejemplo2.s

--- Ejemplo2.s
+++ Ejemplo2.s
@@ -1,21 +1,29 @@
-			THUMB
-			AREA 	DATA, ALIGN=4 
-cont		SPACE	4              
-			AREA	|.text|, CODE, READONLY, ALIGN=2
-			EXPORT	Start
+; Un contador que cuenta hasta 10 y guarda el valor del contador en una variable
+
+
+;-------------------Define el uso de instrucciones THUMB-----------
+		THUMB
+;-----------Define area para datos de 4 bytes---------------
+		AREA 	DATA, ALIGN=4 
+;-----------Declarar Variables -------------------------
+cont		SPACE	4
+;----------Define área de codigo, con capacidad de ser importado C---------
+		AREA	|.text|, CODE, READONLY, ALIGN=2
+;----------Declara la instrucción Start como Global-----------			
+		EXPORT	Start
+;----------------------Codigo------------------------------------
+
+Start		MOV R1, #1	;R1 = 1
+cuenta		ADD R1, #1	;R1 = R1+1
+		CMP R1, #10	; compara el R1 y 10 y actualiza las banderas 
+		BEQ fin		; si son iguales(EQ) R1 Y el valor comparado, salta hacia "fin", sino ejecuta la siguiente instruccion
+		B cuenta	; salta a cuenta
+
+fin		LDR R0, =cont	;R0 = *cont, guarda la direccion del espacio de memoria asignado a la variable cont
+		STR R1, [R0]    ;cont = R1, asigna el valor de R1 a la variable cont, en memoria
+		B Start		; regresa Start
 
 			
-Start		MOV R1, #1
-cuenta		ADD R1, #1
-			MUL R1, #10
-			CMP R1, #10
-			BEQ fin
-			B cuenta
-
-fin			LDR R0, =cont
-			STR R1, [R0]
-			B Start
-
-			
-			ALIGN      
-			END 
+		ALIGN      
+		END
+	
