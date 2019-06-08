f170cfa55356a7eb0d32dbebeb7acbb95ddc7c58
oasm95 <41705782+oasm95@users.noreply.github.com>
2019-06-07 18:11:17 -0600

    Update Ejemplo3-1.s

--- Ejemplo3-1.s
+++ Ejemplo3-1.s
@@ -25,7 +25,7 @@ Start	  MOV R1, #1	    ;R1 = 1
 			
 Factorial MUL R1,R4	      ;R1 = R1*R4
           SUBS R4, #1	    ;R4 = R4 - 1, el sufijo S en SUB, actualiza las banderas del registro SPR
-          BEQ fin		      ;Como CMP Rn, Rd = SUB Rn, Rd, al obtener un cero de resultado la condicion EQ correspondiente a Z = 1, realiza el salto
+          BEQ fin	    ;Como CMP Rn, Rd = SUB Rn, Rd, al obtener un cero de resultado la condicion EQ correspondiente a Z = 1, realiza el salto
           B Factorial	    ;sino realiza otra iteracion
 
 fin2	  MOV R1, #1	    ;si se desea calcular el factorial de 0 automaticamente se le asigna al resultado un valor de 1, 0!=1
