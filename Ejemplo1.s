5c66a9ff5100f9766a15417cdab15025bd360f38
oasm95 <41705782+oasm95@users.noreply.github.com>
2019-06-09 19:16:22 -0600

    Update Ejemplo1.s

--- Ejemplo1.s
+++ Ejemplo1.s
@@ -13,14 +13,14 @@ Start			MOV R1, #0x1F; 		R1 = 0x1F
 			MOV R2, #0X1A00; 	R2 = 0x1A00
 			MOV R3, R1;		R3 = R1
 			
-			LDR R0 ,=cont		; R0 = *cont, Guarda la direccion en donde se encuentra la variable cont en R0
-			LDR R4 , [R0]		; R4 = &R0,  Carga el valor de la variable cont en R4 
+			LDR R0 ,=cont		; R0 = &cont, Guarda la direccion en donde se encuentra la variable cont en R0
+			LDR R4 , [R0]		; R4 = *R0,  Carga el valor de la variable cont en R4 
 			
 			ORR R1, R2 		; R1 = R1 OR R2 = 0X1A1F
 			
-			STR R1, [R0]		; cont = R1, Guarda el valor de R1 en la direccion de R0
+			STR R1, [R0]		; *R0 = R1, Guarda el valor de R1 en la direccion de R0
 			
-			LDR R4,[R0]		; R4 = &R0,  Carga el valor de la variable cont en R4 
+			LDR R4,[R0]		; R4 = *R0,  Carga el valor de la variable cont en R4 
 
 			
 			ALIGN      
