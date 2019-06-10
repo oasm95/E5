3829475f9f4cf69d3fe0fb379cd6fdd63a8a26fc
oasm95 <41705782+oasm95@users.noreply.github.com>
2019-06-09 19:19:05 -0600

    Update Ejemplo3-1.s

--- Ejemplo3-1.s
+++ Ejemplo3-1.s
@@ -21,16 +21,16 @@ resultado SPACE	4
 Start	  MOV R1, #1	    ;R1 = 1
           MOV R4, #5	    ;R4 = 5	
           CMP R4, #0	    ;realiza una comparacion entre R4 y 0
-          BEQ fin2	      ;Si son iguales salta hacia fin2 
+          BEQ fin2	    ;Si son iguales salta hacia fin2 
 			
-Factorial MUL R1,R4	      ;R1 = R1*R4
+Factorial MUL R1,R4	      ;R1 = R1xR4
           SUBS R4, #1	    ;R4 = R4 - 1, el sufijo S en SUB, actualiza las banderas del registro SPR
           BEQ fin	    ;Como CMP Rn, Rd = SUB Rn, Rd, al obtener un cero de resultado la condicion EQ correspondiente a Z = 1, realiza el salto
           B Factorial	    ;sino realiza otra iteracion
 
-fin2	  MOV R1, #1	    ;si se desea calcular el factorial de 0 automaticamente se le asigna al resultado un valor de 1, 0!=1
-fin       LDR R3, =resultado ; R3 = *resultado, guarda la direccion del espacio de memoria asignado a la variable resultado en R3
-          STR R1, [R3]	  ;asigna el valor de R1 a la localidad de memoria que apunta R3
+fin2	  MOV R1, #1	     ;si se desea calcular el factorial de 0 automaticamente se le asigna al resultado un valor de 1, 0!=1
+fin       LDR R3, =resultado ; R3 = &resultado, guarda la direccion del espacio de memoria asignado a la variable resultado en R3
+          STR R1, [R3]	     ;*R3 = R1, asigna el valor de R1 a la localidad de memoria que apunta R3
           B Start
 
           ALIGN      
