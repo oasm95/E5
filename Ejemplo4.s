ca247a54e982ffac5cc8997c4f5e638cb4db6766
oasm95 <41705782+oasm95@users.noreply.github.com>
2019-06-13 16:07:38 -0600

    Update Ejemplo4.s

--- Ejemplo4.s
+++ Ejemplo4.s
@@ -1,5 +1,5 @@
-; Implementacion de la funcion factorial, guarda el resultado de factorial en una variable
-
+; Inicializacion de puerto F, Habilitar pines PF 1, 2 y 3 como salidas y encender los tres pines.
+; Para encender una luz blanca
 ;-------------------Define el uso de instrucciones THUMB-----------
 		THUMB
 ;-----------Define area para datos de 4 bytes---------------
@@ -16,39 +16,38 @@ resultado	SPACE	4
 Start   
 
 Int_PF		LDR R1, =0X400FE608
-			LDR R0 , [R1]
-			ORR R0, #0X20
-			STR R0, [R1]
-			NOP
-			NOP
-			NOP
+		LDR R0 , [R1]
+		ORR R0, #0X20
+		STR R0, [R1]
+		NOP
+		NOP
+		NOP
 ;---------DESACTIVAR FUNCIONES ANALOGICAS AMSEL			
-			LDR R1, =0X40025000
-			LDR R0, [R1,#0X528]
-			BIC R0, #0XFF
-			STR R0, [R1,#0X528]
+		LDR R1, =0X40025000
+		LDR R0, [R1,#0X528]
+		BIC R0, #0XFF
+		STR R0, [R1,#0X528]
 ;--------ASIGNAR ENTRADAS Y SALIDAS, DIR---
-			LDR R0, [R1, #0X400]
-			ORR R0, #0X0E; PIN 2,3,4 COMO SALIDAS
-			STR R0, [R1,#0X400]
+		LDR R0, [R1, #0X400]
+		ORR R0, #0X0E; PIN 2,3,4 COMO SALIDAS
+		STR R0, [R1,#0X400]
 ;-----ASIGNAR FUNCION ALTERNATIVA,PCTL (GPI0 = 0)
-			MOV R0, #0
-			STR R0, [R1, #0X52C]
+		MOV R0, #0
+		STR R0, [R1, #0X52C]
 ;-----DESABILITAR FUNCION ALTERNATIVA, AFSEL
-			LDR R0, [R1, #0X420]
-			BIC R0, #0XFF
-			STR R0, [R1, #0X420]
+		LDR R0, [R1, #0X420]
+		BIC R0, #0XFF
+		STR R0, [R1, #0X420]
 ;-------HABILITAR FUNCIONES DIGITALES, DEN
-			LDR R0, [R1, #0X51C]
-			ORR R0, #0X0E
-			STR R0, [R1, #0X51C]
+		LDR R0, [R1, #0X51C]
+		ORR R0, #0X0E
+		STR R0, [R1, #0X51C]
 
 ;-------PRENDER LEDS---------
 LOOP
-			LDR R0, [R1, #0X3FC]
-			ORR R0, #0X0E
-			STR R0, [R1, #0X3FC]
-			B LOOP
-
+		LDR R0, [R1, #0X3FC]
+		ORR R0, #0X0E
+		STR R0, [R1, #0X3FC]
+		B LOOP
 		ALIGN      
 		END 
